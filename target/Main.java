a31f1c74b236522308b0801fc29c4d996f67a989
swapkumar <kumawat.swapnil@gmail.com>
2025-10-17 21:42:28 -0400

    Initial commit

---
+++ target/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+public class Main{
+
+	public static void main (String[] args ){
+
+	System.out.println("This java Program");
+System.out.println(new Date());
+		}
+}
---
+++ target/MyApplication/index.jsp
@@ -0,0 +1,7 @@
+<html>
+<body>
+<h2>Hello Swapnil!</h2>
+<h3> This is my first java application after long time</h3>
+<a href="about.jsp">About</a></a>
+</body>
+</html>
